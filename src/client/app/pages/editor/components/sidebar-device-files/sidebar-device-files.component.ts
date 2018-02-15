9cb8d6a3d436aac484df5d5f5d398e2e943ae4b0
Brian J Jones <brian.j.jones@intel.com>
2018-02-14 17:24:03 -0800

    Latest

--- src/client/app/pages/editor/components/sidebar-device-files/sidebar-device-files.component.ts
+++ src/client/app/pages/editor/components/sidebar-device-files/sidebar-device-files.component.ts
@@ -1,14 +1,14 @@
 import { Component, EventEmitter, Output, OnInit } from '@angular/core';
-// import { WebUsbService } from '../../../../shared/webusb/webusb.service';
-// import { WebUsbPort } from '../../../../shared/webusb/webusb.port';
-// import { SettingsService } from '../../settings.service';
+import { WebUsbService } from '../../../../shared/webusb/webusb.service';
+//import { WebUsbPort } from '../../../../shared/webusb/webusb.port';
+//import { SettingsService } from '../../settings.service';
 
 @Component({
     moduleId: module.id,
     selector: 'sd-sidebar-device-files',
     templateUrl: 'sidebar-device-files.component.html',
-    styleUrls: ['sidebar-device-files.component.css']
-//    providers: [WebUsbService, SettingsService]
+    styleUrls: ['sidebar-device-files.component.css'],
+    providers: [WebUsbService] //, SettingsService]
 })
 export class SidebarDeviceFilesComponent implements OnInit {
     @Output()
@@ -19,44 +19,46 @@ export class SidebarDeviceFilesComponent implements OnInit {
 
     private fileCount : number = 0;
     private fileArray = [];
+    //private webusbService;
+    //private webusbService: WebUsbService;
     // subscription: Subscription;
-    //constructor(){}
-    // constructor(private settingsService: SettingsService,
-    //             private webusbService: WebUsbService) { }
-
+    public constructor(//private settingsService: SettingsService,
+                public webusbService: WebUsbService
+            ) { }
+    // constructor () {
+    //     this.webusbService = WebUsbService;
+    // }
     ngOnInit() {
         this.getFileInfo();
     }
 
     // tslint:disable-next-line:no-unused-locals
     public onDeviceFilenameClicked(filename: string) {
-    //     this.webusbService.load(filename)
-    //     .then(async (res) => {
-    //     this.onFileSelected.emit({
-    //         filename: filename,
-    //         contents: res
-    //     });
-    // });
-        return false;
+        this.webusbService.load(filename)
+        .then(async (res) => {
+        this.onFileSelected.emit({
+            filename: filename,
+            contents: res
+        });
+    });
     }
 
     // tslint:disable-next-line:no-unused-locals
     public onDeleteDeviceFileClicked(filename: string) {
         let that = this;
-        // this.webusbService.rm(filename)
-        // .then(async (res) => {
-        //     that.onDeviceFileDeleted.emit(filename);
-        //     that.getFileInfo();
-        // });
-        return false;
+        this.webusbService.rm(filename)
+        .then(async (res) => {
+            that.onDeviceFileDeleted.emit(filename);
+            that.getFileInfo();
+        });
     }
 
     private getFileInfo() {
-        // let deviceThis = this;
-        // this.webusbService.lsArray()
-        // .then( function (arr) {
-        //     deviceThis.fileArray = arr;
-        //     deviceThis.fileCount = deviceThis.fileArray.length;
-        // });
+        let deviceThis = this;
+        this.webusbService.lsArray()
+        .then( function (arr) {
+            deviceThis.fileArray = arr;
+            deviceThis.fileCount = deviceThis.fileArray.length;
+        });
     }
 }
